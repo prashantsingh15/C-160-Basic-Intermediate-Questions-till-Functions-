7d0ea1b334fd34d62b66f471a1e43537a90d9e0c
prashantsingh15 <prashantsingh15092002@gmail.com>
2024-08-10 12:32:24 +0530

    main

---
+++ Basic Programming Phas/Question2.cpp
@@ -0,0 +1,12 @@
+// WAP TO ENTER A NUMBER AND RETURN IT AS THE OUTPUT..
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the number Here: \n";
+    cin>>a;
+    cout<<"The Entered Number is : " <<a;
+
+
+}
