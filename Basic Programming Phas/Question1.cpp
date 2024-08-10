6dab1fe26700254ade652e1c829b2af6a06e42af
prashantsingh15 <prashantsingh15092002@gmail.com>
2024-08-10 12:30:29 +0530

    commit

--- Basic Programming Phas/Question1.cpp
+++ Basic Programming Phas/Question1.cpp
@@ -1,2 +1,13 @@
 // WAP to use the sizeof Operator.
 
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    char c;
+    cout<<"Size of a is "<<sizeof(a)<<"\n";
+    cout<<"Size of a is "<<sizeof(b)<<"\n";
+    cout<<"Size of a is "<<sizeof(c)<<"\n";
+
+}
