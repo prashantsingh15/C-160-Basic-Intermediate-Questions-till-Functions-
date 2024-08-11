7471241126076de345b66206206b04a1c67365c1
prashantsingh15 <prashantsingh15092002@gmail.com>
2024-08-11 14:18:45 +0530

    commit

---
+++ Basic Programming Phas/Question3.cpp
@@ -0,0 +1,16 @@
+// WAP for and Operator...
+
+#include <iostream>
+using namespace std;
+int main(){
+    bool a = true;
+    bool b = false;
+    if(a && b){
+        cout<<"Both a and B are true";
+
+
+    }
+    else{
+        cout<<"Out";
+    }
+}
---
+++ Basic Programming Phas/Question4.cpp
@@ -0,0 +1,15 @@
+// Wap for or operator.
+
+#include <iostream>
+using namespace std;
+int main(){
+    bool a = true;
+    bool b = false;
+    if(a || b){
+        cout << "A and b will return true";
+
+    }
+    else{
+        cout<<"Else Block ";
+    }
+}
