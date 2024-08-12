81b0dd15343eca363a5a6387891b14dd10f0d83f
prashantsingh15 <prashantsingh15092002@gmail.com>
2024-08-12 10:18:37 +0530

    commit

---
+++ Basic Programming Phas/Question5.cpp
@@ -0,0 +1,21 @@
+//  Wap for assignment operator....
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+    a = 10;
+    b = 12;
+
+    cout<< "a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    a = b;
+
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+
+    return 0;
+
+
+}
+
